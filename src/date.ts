a33387289cf957f5ce504ced2d5ff602d21b6dbc
Pooya Parsa <pooya@pi0.io>
2024-06-10 17:31:28 +0200

    refactor: improve date utils

--- src/date.ts
+++ src/date.ts
@@ -7,12 +7,20 @@
  * formatDateString(new Date("2021/01/01")) // "2021-01-01"
  * ```
  */
-export function formatDateString(date: string | Date): DateString {
-  const d = new Date(date);
+export function formatDateString(date: DateValue): DateString {
+  const d = normalizeDate(date);
   const year = d.getFullYear().toString();
   const month = (d.getMonth() + 1).toString().padStart(2, "0");
   const day = d.getDate().toString().padStart(2, "0");
   return `${year}-${month}-${day}` as DateString;
+}
+// --- internal ---
+
+function normalizeDate(date: DateValue): Date {
+  if (date instanceof Date) {
+    return date;
+  }
+  return new Date(date);
 }
 
 // --- Types ---
@@ -25,3 +33,5 @@ type Day = `${"0" | "1" | "2" | "3"}${number}`;
  * Typed date string in `YYYY-MM-DD` format
  */
 export type DateString = `${Year}-${Month}-${Day}`;
+
+export type DateValue = Date | DateString;
--- test/date.test.ts
+++ test/date.test.ts
@@ -1,14 +1,14 @@
 import { expect, it, describe } from "vitest";
-import { formatDateString } from "../src";
+import { formatDateString, DateString, DateValue } from "../src/date";
 
-describe("compatx", () => {
+describe("date utils", () => {
   describe("formatDateString", () => {
     const cases = [
       ["2021-12-31", "2021-12-31"],
       ["2021/12/31", "2021-12-31"],
       [new Date("2021/01/01"), "2021-01-01"],
       [new Date("2021/01/01").toString(), "2021-01-01"],
-    ];
+    ] as [DateValue, DateString][];
     for (const [date, formattedDate] of cases) {
       it(`should format ${date} to ${formattedDate}`, () => {
         expect(formatDateString(date)).toBe(formattedDate);
