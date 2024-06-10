4d7a861fd3ee627f4ef0bbb49c693aa037330faa
Pooya Parsa <pooya@pi0.io>
2024-06-10 18:06:36 +0200

    chore: update test

--- test/date.test.ts
+++ test/date.test.ts
@@ -1,17 +1,17 @@
 import { expect, it, describe } from "vitest";
-import { formatDateString, DateString, DateValue } from "../src/date";
+import { formatDate, DateString } from "../src/date";
 
 describe("date utils", () => {
-  describe("formatDateString", () => {
+  describe("formatDate", () => {
     const cases = [
       ["2021-12-31", "2021-12-31"],
       ["2021/12/31", "2021-12-31"],
       [new Date("2021/01/01"), "2021-01-01"],
       [new Date("2021/01/01").toString(), "2021-01-01"],
-    ] as [DateValue, DateString][];
+    ] as [Date | string, DateString][];
     for (const [date, formattedDate] of cases) {
       it(`should format ${date} to ${formattedDate}`, () => {
-        expect(formatDateString(date)).toBe(formattedDate);
+        expect(formatDate(date)).toBe(formattedDate);
       });
     }
   });
